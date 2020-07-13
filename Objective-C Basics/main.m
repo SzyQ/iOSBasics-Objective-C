69b1bede7b0ff4188d81088e19e15a20b4524be7
Szymon Klimaszewski <szymon@unum.la>
2020-07-13 23:35:08 +0200

    Exercise 3 - optional method

--- Objective-C Basics/main.m
+++ Objective-C Basics/main.m
@@ -18,6 +18,9 @@ int main(int argc, const char * argv[]) {
         if([person respondsToSelector:storySelector]) {
             [person tellMeAnotherStory];
         }
+        if([person respondsToSelector:@selector(considerTellingMeAnotherStory)]){
+            [person considerTellingMeAnotherStory];
+        }
     }
     return 0;
 }
