387c8e8349e07b74e9a94f1d3b0abed11575dfe3
Szymon Klimaszewski <szymon@unum.la>
2020-07-13 21:11:22 +0200

    Exercise 1 - Data types

--- Objective-C Basics/main.m
+++ Objective-C Basics/main.m
@@ -10,8 +10,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"char: %lu, short int: %lu, int: %lu, long int: %lu, float: %lu, double: %lu.",
+        sizeof(short int),sizeof(int),sizeof(char),sizeof(long int),sizeof(float),sizeof(double));
+        
+        char arrayOfCharacter[] = "Ciąg znaków";
+        NSLog(@"%s",arrayOfCharacter);
+        
+        NSLog(@"Skoro if(5) -> TRUE, to if(5 == TRUE) -> %d",5 == TRUE);
+        
+        NSLog(@"Dzielenie 1/5=%d, zaś (float)1/5=%.2f",1/5, (float)1/5);
+        
+        typedef struct Point { int x; int y;} Point;
+        Point test = {2,4};
+        NSLog(@"Punkt(%d,%d)",test.x, test.y);
     }
     return 0;
 }
