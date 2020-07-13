990c235a440c60edab62373b872b2ffe6868e62a
Szymon Klimaszewski <szymon@unum.la>
2020-07-13 20:41:43 +0200

    Initial Commit

---
+++ Objective-C Basics/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C Basics
+//
+//  Created by Work on 13/07/2020.
+//  Copyright © 2020 Szymon Klimaszewski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
